be8206529e42267afb80ea779b67bd83fcadf63b
Lucas Viana <lucasvfv@gmail.com>
2023-01-31 11:07:36 -0300

    feature

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req,res) => {
     res.send('Server rodando normal');
 })
-
+console.log("teste")
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
