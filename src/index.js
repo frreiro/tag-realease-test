d4235007d4f784486dffbe9165fdae37a98fe0e8
Lucas Viana <lucasvfv@gmail.com>
2023-03-07 18:08:16 -0300

    feat: add env file in actions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+const dotenv = require('dotenv')
+const path = require('path')
+
+const env = dotenv.config({
+    path: path.join(__dirname,'../.env.production')
+});
+
 setInterval(( ) => {
     
     // console.log("teste")
-    //  console.log("teste")
+    console.log(process.env.NODE_ENV)
     console.log("Amor")
 },1000)
